5263c3b2edbac3a71d84d8c1ff1e681b395cf4e7
Manrriquez <manrriquez.contato@gmail.com>
2022-11-30 20:05:37 -0300

    projeto criado

---
+++ src/main/java/com/registerPerson/RegisterPersonApplication.java
@@ -0,0 +1,13 @@
+package com.registerPerson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterPersonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterPersonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/registerPerson/RegisterPersonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.registerPerson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterPersonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
