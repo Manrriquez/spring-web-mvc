38b542790cfd802d05ce1bffaef4e262f844909f
Manrriquez <manrriquez.contato@gmail.com>
2022-12-01 00:22:45 -0300

    implementando metodo PUT, atualizar Pessoa

--- src/main/java/com/registerPerson/controllers/PersonController.java
+++ src/main/java/com/registerPerson/controllers/PersonController.java
@@ -4,10 +4,11 @@ package com.registerPerson.controllers;
 import com.registerPerson.models.PersonModel;
 import com.registerPerson.repositories.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
 
 
 @Controller
@@ -18,29 +19,42 @@ public class PersonController {
 
     //@GetMapping(value = "registerPerson")
     @RequestMapping(value = "/registerPerson", method = RequestMethod.GET)
-    public String init() {
-        return "register/registerPerson";
+    public ModelAndView init() {
+        ModelAndView modelAndView = new ModelAndView("register/registerPerson");
+        modelAndView.addObject("personobj", new PersonModel());
+        return modelAndView;
     }
 
-    //    @PostMapping(value = "savePerson")
-    @RequestMapping(value = "/savePerson", method = {RequestMethod.GET, RequestMethod.POST})
+//    @PostMapping(value = "/savePerson")
+    @RequestMapping(value = "**/savePerson", method = {RequestMethod.GET, RequestMethod.POST})
     public ModelAndView savePerson(PersonModel person) {
 
         personRepository.save(person);
         ModelAndView andView = new ModelAndView("register/registerPerson");
         Iterable<PersonModel> personsIt = personRepository.findAll();
         andView.addObject("persons", personsIt);
+        andView.addObject("personobj", new PersonModel());
         return andView;
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "listPerson")
+    @RequestMapping(method = RequestMethod.GET, value = "/listPerson")
     public ModelAndView persons() {
         ModelAndView andView = new ModelAndView("register/registerPerson");
 
         Iterable<PersonModel> personsIt = personRepository.findAll();
         andView.addObject("persons", personsIt);
+        andView.addObject("personobj", new PersonModel());
         return andView;
     }
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "editPerson/{idPerson}")
+    public ModelAndView editPerson(@PathVariable("idPerson") Long idPerson) {
+        Optional<PersonModel> person = personRepository.findById(idPerson);
+        ModelAndView modelAndView = new ModelAndView("register/registerPerson");
+        modelAndView.addObject("personobj", person.get());
+
+        return  modelAndView;
+    }
+
 
 
 }
